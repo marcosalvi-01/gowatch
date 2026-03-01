f9b34a9bb841ab0e7d0866b18ef27346b9db2ad0
Marco Salvi <1marco.salvi@gmail.com>
2026-03-01 01:15:05 +0100

    chore(ui): fix watchlist header on mobile

--- internal/ui/components/liststats/liststats_templ.go
+++ internal/ui/components/liststats/liststats_templ.go
@@ -115,7 +115,7 @@ func watchlistListStat(label, value string) templ.Component {
 			templ_7745c5c3_Var2 = templ.NopComponent
 		}
 		ctx = templ.ClearChildren(ctx)
-		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "<div class=\"text-center sm:text-left\"><div class=\"text-xl font-bold text-primary\">")
+		templ_7745c5c3_Err = templruntime.WriteString(templ_7745c5c3_Buffer, 7, "<div class=\"text-left\"><div class=\"text-xl font-bold text-primary\">")
 		if templ_7745c5c3_Err != nil {
 			return templ_7745c5c3_Err
 		}
